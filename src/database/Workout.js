b904694e9e0e01a0e230742fdbc0bbb1031b2dfe
Mario <sks_894@hotmail.com>
2023-03-24 09:48:46 -0300

    Create functionality: add new workout to database

--- src/database/Workout.js
+++ src/database/Workout.js
@@ -1,9 +1,23 @@
 const DB = require('./db.json');
+const { saveToDatabase } = require('./utils');
 
 function getAllWorkouts() {
   return DB.workouts;
 }
 
+function createNewWorkout(newWorkout) {
+  const isAlreadyAdded = DB.workouts.findIndex(workout => workout.name === newWorkout.name);
+
+  if (isAlreadyAdded) return;
+
+  DB.workouts.push(newWorkout);
+
+  saveToDatabase(DB);
+
+  return newWorkout;
+}
+
 module.exports = {
-  getAllWorkouts
+  getAllWorkouts,
+  createNewWorkout
 };
--- src/services/workoutService.js
+++ src/services/workoutService.js
@@ -1,3 +1,4 @@
+const { v4: uuid } = require('uuid');
 const Workout = require('../database/Workout');
 
 function getAllWorkouts() {
@@ -9,8 +10,17 @@ function getOneWorkout() {
   return;
 }
 
-function createNewWorkout() {
-  return;
+function createNewWorkout(newWorkout) {
+  const workoutToInsert = {
+    ...newWorkout,
+    id: uuid(),
+    createdAt: new Date().toLocaleString('en-US', { timeZone: 'UTC' }),
+    updatedAt: new Date().toLocaleString('en-US', { timeZone: 'UTC' })
+  };
+
+  const createdWorkout = Workout.createNewWorkout(workoutToInsert);
+
+  return createdWorkout;
 }
 
 function updateOneWorkout() {
